46389c8c4209de68c3fcc56788839c3e7935108f
kalyan <kalyan.chaval123@gmail.com>
2016-10-30 18:26:19 -0400

    Frills to the homepage

--- templates/shout/static/js/homeScript.js
+++ templates/shout/static/js/homeScript.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-		
+	$(".shout_text").keyup(function(){
+		text = $(this).val();
+		$(".charNum").text(text.length);
+		if(text.length > 160){
+			$(".charNum").css("color","red");
+			$("#shoutBtn").prop('disabled',true);
+		}else{
+			$(".charNum").css("color","black");
+			$("#shoutBtn").prop('disabled',false);
+		}
+	});
+
+	
 });
