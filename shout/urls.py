366fec6b9224d06b3bb20b9dad8e2664349d6ba8
kalyan <kalyan.chaval123@gmail.com>
2016-11-10 17:45:41 -0500

    Profile page

--- shout/urls.py
+++ shout/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
 	url(r'^logout/$', views.user_logout, name="logout"),
 	url(r'^shout/$', views.shout, name="shout"),
 	url(r'^createEvent/$', views.events, name="event"),
+	url(r'^profile/$', views.profile_view, name="profile"),
 ]
