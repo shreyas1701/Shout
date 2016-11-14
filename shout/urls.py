e70b324f20cbfc934982fcbfa273c5d2bd910d21
kalyan <kalyan.chaval123@gmail.com>
2016-11-14 09:30:58 -0500

    "resolving Sprint 2 merge conflicts"

--- shout/urls.py
+++ shout/urls.py
@@ -3,13 +3,14 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name="index"),
-	url(r'^register/$', views.register, name="register"),
-	url(r'^login/$', views.user_login, name="login"),
-	url(r'^logout/$', views.user_logout, name="logout"),
-	url(r'^shout/$', views.shout, name="shout"),
-	url(r'^createEvent/$', views.events, name="event"),
-	url(r'^profile/$', views.profile_view, name="profile"),
-	url(r'^notify/$', views.notify, name="notify"),
-	url(r'^edit_event/(?P<id>\d+)/$', views.edit_event, name='edit_event'),
+    url(r'^$', views.index, name="index"),
+    url(r'^register/$', views.register, name="register"),
+    url(r'^login/$', views.user_login, name="login"),
+    url(r'^logout/$', views.user_logout, name="logout"),
+    url(r'^shout/$', views.shout, name="shout"),
+    url(r'^createEvent/$', views.events, name="event"),
+    url(r'^profile/$', views.profile_view, name="profile"),
+    url(r'^notify/$', views.notify, name="notify"),
+    url(r'^edit_event/(?P<id>\d+)/$', views.edit_event, name='edit_event'),
+    url(r'^myevents/$', views.eventsview, name="eventsview"),
 ]
